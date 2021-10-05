a539152b4d7ef8a93bb879ba539ca557ee7e2d03
Biz Melesse <bizmelesse@gmail.com>
2021-10-05 00:07:25 -0700

    Render calendar and event items together

--- app/src/main/java/io/learnet/app/adapter/EventHomeAdapter.kt
+++ app/src/main/java/io/learnet/app/adapter/EventHomeAdapter.kt
@@ -21,6 +21,9 @@ class EventHomeAdapter(private val viewItems: List<*>) :
 
     companion object {
         private const val TYPE_EVENT_CALENDAR = 0
+        private const val TYPE_EVENT_DATE = 1
+        private const val TYPE_EVENT_ITEM = 2
+        private const val TYPE_EVENT_PLACEHOLDER = 3
     }
 
     inner class EventCalendarViewHolder(itemView: View) : BaseViewHolder<EventCalendar>(itemView) {
@@ -32,12 +35,44 @@ class EventHomeAdapter(private val viewItems: List<*>) :
         }
     }
 
+    inner class EventItemViewHolder(itemView: View) : BaseViewHolder<EventItem>(itemView) {
+        override fun bind(item: EventItem) {
+            val eventTitleView = itemView.findViewById<TextView>(R.id.tv_event_title)
+            val eventSummaryView = itemView.findViewById<TextView>(R.id.tv_event_summary)
+            eventTitleView.text = item.title
+            eventSummaryView.text = item.summary
+        }
+    }
+
+    inner class EventPlaceholderViewHolder(itemView: View) : BaseViewHolder<EventPlaceholder>(itemView) {
+        override fun bind(item: EventPlaceholder) {
+            val eventTitleView = itemView.findViewById<TextView>(R.id.tv_event_placeholder)
+            eventTitleView.text = item.value
+        }
+    }
+
+    inner class EventSectionHeaderViewHolder(itemView: View) : BaseViewHolder<SectionHeader>(itemView) {
+        override fun bind(item: SectionHeader) {
+            val headerView = itemView.findViewById<TextView>(R.id.tv_event_date)
+            headerView.text = item.title
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
         val inflater = LayoutInflater.from(parent.context)
         return when (viewType) {
             TYPE_EVENT_CALENDAR -> {
                 val eventCalendarView = inflater.inflate(R.layout.event_item_calendar, parent, false)
                 EventCalendarViewHolder(eventCalendarView)
+            } TYPE_EVENT_PLACEHOLDER -> {
+                val eventPlaceholderView = inflater.inflate(R.layout.event_placeholder, parent, false)
+                EventPlaceholderViewHolder(eventPlaceholderView)
+            } TYPE_EVENT_ITEM -> {
+                val eventItemView = inflater.inflate(R.layout.fragment_event_item, parent, false)
+                EventItemViewHolder(eventItemView)
+            } TYPE_EVENT_DATE -> {
+                val eventDateView = inflater.inflate(R.layout.event_date_header, parent, false)
+                EventSectionHeaderViewHolder(eventDateView)
             } else -> throw IllegalArgumentException("Invalid view type")
         }
     }
@@ -45,6 +80,9 @@ class EventHomeAdapter(private val viewItems: List<*>) :
     override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
         when (holder) {
             is EventCalendarViewHolder -> holder.bind(viewItems[position] as EventCalendar)
+            is EventListAdapter.EventItemViewHolder -> holder.bind(viewItems[position] as EventItem)
+            is EventListAdapter.EventSectionHeaderViewHolder -> holder.bind(viewItems[position] as SectionHeader)
+            is EventListAdapter.EventPlaceholderViewHolder -> holder.bind(viewItems[position] as EventPlaceholder)
         }
     }
 
@@ -55,6 +93,9 @@ class EventHomeAdapter(private val viewItems: List<*>) :
     override fun getItemViewType(position: Int): Int {
         return when (viewItems[position]) {
             is EventCalendar -> TYPE_EVENT_CALENDAR
+            is SectionHeader -> TYPE_EVENT_DATE
+            is EventItem -> TYPE_EVENT_ITEM
+            is EventPlaceholder -> TYPE_EVENT_PLACEHOLDER
             else -> throw IllegalArgumentException("Invalid type of data $position")
         }
     }
--- app/src/main/java/io/learnet/app/ui/events/EventHomeFragment.kt
+++ app/src/main/java/io/learnet/app/ui/events/EventHomeFragment.kt
@@ -9,8 +9,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.learnet.app.R
 import io.learnet.app.adapter.EventHomeAdapter
+import io.learnet.app.ui.utils.SectionHeader
 
 class EventHomeFragment : Fragment() {
+    val events = DemoEventCreator.createEventList(20)
+//    val events = DemoEventCreator.createEmptyEvent();
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,11 +23,15 @@ class EventHomeFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
         val rvEventHome = view?.findViewById(R.id.rv_event_home) as RecyclerView
         val eventHomeItems = ArrayList<Any>()
         eventHomeItems.add(DemoEventCreator.createEventHomeCalendar())
+        if (events.events.size > 0) {
+            eventHomeItems.add(SectionHeader(events.date))
+            eventHomeItems.addAll(events.events)
+        } else {
+            eventHomeItems.add(EventPlaceholder("${getString(R.string.event_placeholder)} ${events.date}"))
+        }
         val adapter = EventHomeAdapter(eventHomeItems)
         rvEventHome.adapter = adapter
         rvEventHome.layoutManager = LinearLayoutManager(activity)
